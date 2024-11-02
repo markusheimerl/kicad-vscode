17027735154916c263297dba68ce681905f88158
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-11-02 09:27:30 +0000

    try out schemdraw

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from skidl import *
+import schemdraw
+from schemdraw import elements as elm
 
-# Create net stubs.
-e, b, c = Net("ENET"), Net("BNET"), Net("CNET")
-e.stub, b.stub, c.stub = True, True, True
+# Create a drawing
+d = schemdraw.Drawing()
 
-# Create transistor part template.
-qt = Part(lib="Device.lib", name="Q_PNP_CBE", dest=TEMPLATE)
+# Add circuit elements
+d += (V1 := elm.SourceV().label('Vin').up())
+d += (R1 := elm.Resistor().right().label('R1'))
+d += (dot1 := elm.Dot())
+d += elm.Resistor().down().label('R2')
+d += elm.Line().left()
+d += elm.Ground()
+d += elm.Line().up()
 
-# Instantiate transistor with various orientations.
-for q, tx in zip(qt(8), ['', 'H', 'V', 'R', 'L', 'VL', 'HR', 'LV']):
-    q['E B C'] += e, b, c  # Attach stubs to transistor pins.
-    q.symtx = tx  # Assign orientation to transistor attributes.
-    q.ref = 'Q_' + tx  # Place orientation in transistor reference.
-
-generate_svg()
+# Save as PNG
+d.save('voltage_divider.png')
